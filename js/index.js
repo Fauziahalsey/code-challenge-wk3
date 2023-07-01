dad0078ea3186a94b65c30d4ac6b6d72eaa4250c
Fauziahalsey <fauziaomala19@gmail.com>
2023-07-02 00:54:33 +0300

    making the button functional

--- js/index.js
+++ js/index.js
@@ -1,93 +1,103 @@
-// Fetch movie details for the first movie
-fetch('http://localhost:3000/films/1')
-  .then(response => response.json())
-  .then(movie => {
-    const { poster, title, runtime, description, showtime, capacity, tickets_sold } = movie;
-    const ticketsAvailable = capacity - tickets_sold;
+// Function to display movie details
+function displayMovieDetails(movie) {
+  const { poster, title, runtime, description, showtime, capacity, tickets_sold } = movie;
+  const ticketsAvailable = capacity - tickets_sold;
 
-    document.getElementById('movie-poster').src = poster;
-    document.getElementById('movie-title').textContent = title;
-    document.getElementById('movie-runtime').textContent = `Runtime: ${runtime} minutes`;
-    document.getElementById('movie-showtime').textContent = `Showtime: ${showtime}`;
-    document.getElementById('movie-tickets').textContent = `Tickets available: ${ticketsAvailable}`;
-    document.getElementById('movie-description').textContent = description;
-  })
-  .catch(error => {
-    console.log('Error:', error);
-  });
+  document.getElementById('movie-poster').src = poster;
+  document.getElementById('movie-title').textContent = title;
+  document.getElementById('movie-runtime').textContent = `Runtime: ${runtime} minutes`;
+  document.getElementById('movie-showtime').textContent = `Showtime: ${showtime}`;
+  document.getElementById('movie-description').textContent = description;
 
-// Fetch movie list
-fetch('http://localhost:3000/films')
-  .then(response => response.json())
-  .then(films => {
-    const filmsList = document.getElementById('films');
+  const buyTicketButton = document.getElementById('buy-ticket');
+  buyTicketButton.disabled = ticketsAvailable === 0;
+  buyTicketButton.textContent = ticketsAvailable === 0 ? 'Sold Out' : 'Buy Ticket';
 
-    films.forEach(film => {
-      const { id, title, poster } = film;
-      const li = document.createElement('li');
-      li.classList.add('film-item');
-      li.innerHTML = `
-        <div>
-          <img src="${poster}" alt="${title} Poster">
-          <h2>${title}</h2>
-        </div>
-      `;
-      filmsList.appendChild(li);
-    });
-  })
-  .catch(error => {
-    console.log('Error:', error);
-  });
-
-// Function to populate film menu
-function populateFilmMenu(films) {
+  // Update film item in the film menu
   const filmsList = document.getElementById('films');
-
-  films.forEach(film => {
-    const { id, title, capacity, tickets_sold } = film;
-    const filmItem = document.createElement('li');
-    filmItem.textContent = title;
-    filmItem.classList.add('film-item');
-
-    if (capacity - tickets_sold === 0) {
-      filmItem.classList.add('sold-out');
-    }
-
-    filmItem.addEventListener('click', () => {
-      displayMovieDetails(film);
-    });
-
-    filmsList.appendChild(filmItem);
-  });
+  const filmItems = filmsList.getElementsByClassName('film-item');
+  for (let i = 0; i < filmItems.length; i++) {
+    filmItems[i].classList.remove('active');
+  }
+  const activeFilmItem = document.getElementById(`film-${movie.id}`);
+  activeFilmItem.classList.add('active');
 }
 
-// Fetch movie details on page load
-window.addEventListener('load', () => {
-  // Fetch first movie's details
-  fetchData('/films/1', movie => {
-    displayMovieDetails(movie);
-  });
-
-  // Fetch all movies and populate the film menu
-  fetchData('/films', films => {
-    populateFilmMenu(films);
-  });
-});
-
-//tickets purchase
-function buyTicket() {
-  const availableTicketsElement = document.getElementById('movie-tickets');
-  let availableTickets = parseInt(availableTicketsElement.textContent.split(': ')[1]);
+// Function to handle ticket purchase
+function buyTicket(movie) {
+  const { id, capacity, tickets_sold } = movie;
+  const availableTickets = capacity - tickets_sold;
 
   if (availableTickets > 0) {
-    availableTicketsElement.textContent = `Available Tickets: ${availableTickets - 1}`;
-  }
-
-  if (availableTickets === 1) {
-    document.getElementById('buy-ticket').disabled = true;
-    document.querySelector('.film-item').classList.add('sold-out');
+    fetch(`http://localhost:3000/films/${id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ tickets_sold: tickets_sold + 1 })
+    })
+      .then(response => response.json())
+      .then(updatedMovie => {
+        displayMovieDetails(updatedMovie);
+      })
+      .catch(error => {
+        console.log('Error:', error);
+      });
   }
 }
 
 // Event listener for buy ticket button
-document.getElementById('buy-ticket').addEventListener('click', buyTicket);
+document.getElementById('buy-ticket').addEventListener('click', function(event) {
+  event.preventDefault();
+
+  fetch('http://localhost:3000/films/1')
+    .then(response => response.json())
+    .then(movie => {
+      buyTicket(movie);
+    })
+    .catch(error => {
+      console.log('Error:', error);
+    });
+});
+
+// Function to populate film menu
+function populateFilmMenu(films) {
+  const filmsList = document.getElementById('films');
+  filmsList.innerHTML = '';
+
+  films.forEach(film => {
+    const { id, title, capacity, tickets_sold } = film;
+    const li = document.createElement('li');
+    li.classList.add('film-item');
+    li.id = `film-${id}`;
+    li.textContent = title;
+    if (capacity - tickets_sold === 0) {
+      li.classList.add('sold-out');
+    }
+
+    li.addEventListener('click', function(event) {
+      event.preventDefault();
+
+      fetch(`http://localhost:3000/films/${id}`)
+        .then(response => response.json())
+        .then(movie => {
+          displayMovieDetails(movie);
+        })
+        .catch(error => {
+          console.log('Error:', error);
+        });
+    });
+
+    filmsList.appendChild(li);
+  });
+}
+
+// Fetch movie list and populate the film menu
+fetch('http://localhost:3000/films')
+  .then(response => response.json())
+  .then(films => {
+    populateFilmMenu(films);
+  })
+  .catch(error => {
+    console.log('Error:', error);
+  });
